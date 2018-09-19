8d2e009c9cbaf4aef8e6e5c0cd6eb723fe8ee6a8
kostalavista <paradoxnumber7@gmail.com>
2018-09-19 09:23:21 +0300

    Update OPK

--- OPK/Crowdsale.sol
+++ OPK/Crowdsale.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
+
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 /**
  * @title SafeMath
@@ -52,7 +54,6 @@ library SafeMath {
 
 contract Ownable {
     address public owner;
-    address public ICOAddress;
 
     event OwnershipTransferred(
         address indexed previousOwner,
@@ -78,20 +79,6 @@ contract Ownable {
 
 
     /**
-     * @dev Throws if called by any account other than the ico.
-     */
-    modifier onlyICO() {
-        require(msg.sender == ICOAddress);
-        _;
-    }
-
-
-    modifier onlyOwnerOrICO() {
-        require(msg.sender == ICOAddress || msg.sender == owner);
-        _;
-    }
-
-    /**
     * @dev Allows the current owner to transfer control of the contract to a newOwner.
     * @param _newOwner The address to transfer ownership to.
     */
@@ -101,10 +88,6 @@ contract Ownable {
         owner = _newOwner;
     }
 
-    function setICOAddress(address _icoAddress) public onlyOwner {
-        require(_icoAddress != address(0));
-        ICOAddress = _icoAddress;
-    }
 }
 
 contract ERC20 {
@@ -113,15 +96,22 @@ contract ERC20 {
     function totalSupply() public view returns (uint256);
     function balanceOf(address who) public view returns (uint256);
     function transfer(address to, uint256 value) public returns (bool);
+    function ownerTransfer(address to, uint256 value) public returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     function approve(address spender, uint256 value) public returns (bool);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function unpause() public returns (bool);
 }
 
 library SafeERC20 {
     function safeTransfer(ERC20 token, address to, uint256 value) internal {
         require(token.transfer(to, value));
+    }
+
+    function safeOwnerTransfer(ERC20 token, address to, uint256 value) internal {
+        require(token.ownerTransfer(to, value));
     }
 
     function safeTransferFrom(ERC20 token, address from, address to, uint256 value) internal {
@@ -147,27 +137,23 @@ library SafeERC20 {
  * the methods to add functionality. Consider using 'super' where appropiate to concatenate
  * behavior.
  */
-contract Crowdsale is Ownable{
+contract Crowdsale is Ownable, usingOraclize{
     using SafeMath for uint256;
     using SafeERC20 for ERC20;
 
     // The token being sold
     ERC20 public token;
-    RefundEscrow private escrow;
     address public wallet;
+    address public reserveFund;
 
     uint256 public openingTime;
     uint256 public closingTime;
 
     uint256 public cap;
+    uint256 public tokensSold;
+    uint256 public tokenPriceInWei;
 
     bool public isFinalized = false;
-
-    // How many token units a buyer gets per wei.
-    // The rate is the conversion between wei and the smallest and indivisible token unit.
-    // So, if you are using a rate of 1 with a DetailedERC20 token with 3 decimals called TOK
-    // 1 wei will give you 1 unit, or 0.001 TOK.
-    uint256 public rate;
 
     // Amount of wei raised
     uint256 public weiRaised;
@@ -184,9 +170,16 @@ contract Crowdsale is Ownable{
     uint public stageCount;
     uint public currentStage;
 
+    mapping (bytes32 => bool) public pendingQueries;
+    mapping (address => bool) public KYC;
 
-    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    uint public oraclizeBalance;
+
+    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 tokens, uint256 bonus);
     event Finalized();
+    event NewOraclizeQuery(string description);
+    event NewKrakenPriceTicker(string price);
+
     /**
      * @dev Reverts if not in crowdsale time range.
      */
@@ -196,45 +189,37 @@ contract Crowdsale is Ownable{
         _;
     }
 
-    /**
-     * @param _rate Number of token units a buyer gets per wei
-     * @param _wallet Address where collected funds will be forwarded to
-     * @param _token Address of the token being sold
-     * @param _openingTime Crowdsale opening time
-     * @param _closingTime Crowdsale closing time
-     */
-    //TODO
-//Soft Cap: $6 million
-//Hard Cap: $30 million
-//Min Purchase: 50 OPK ЭТО С УЧЕТОМ БОСУНА ?
-//Max Purchase: 100 000 OPK  ЭТО С УЧЕТОМ БОСУНА ?
-    constructor(uint256 _rate, address _wallet, ERC20 _token, uint256 _cap,
-                uint256 _openingTime, uint256 _closingTime) public {
-        require(_rate > 0);
+
+    constructor(address _wallet, ERC20 _token, uint256 _cap, uint256 _openingTime, uint256 _closingTime,
+        address _reserveFund, uint256 _tokenPriceInWei) public {
+
         require(_wallet != address(0));
         require(_token != address(0));
+        require(_reserveFund != address(0));
         require(_openingTime >= block.timestamp);
         require(_closingTime >= _openingTime);
         require(_cap > 0);
+        require(_tokenPriceInWei > 0);
 
-
-        rate = _rate;
         wallet = _wallet;
         token = _token;
-
-        escrow = new RefundEscrow(wallet);
+        reserveFund = _reserveFund;
 
         cap = _cap;
         openingTime = _openingTime;
         closingTime = _closingTime;
-
+        tokenPriceInWei = _tokenPriceInWei;
 
         currentStage = 1;
-        addStage(openingTime + 1  days, 2000, 2250, 2500);
-        addStage(openingTime + 8  days, 1500, 1750, 2000);
-        addStage(openingTime + 15  days, 500, 750, 1000);
-        addStage(openingTime + 22  days, 100, 100, 100);
-        addStage(openingTime + 29  days, 0, 0, 0);
+        //TODO change days
+        //        addStage(openingTime + 1  days, 2000, 2250, 2500);
+        //        addStage(openingTime + 8  days, 1500, 1750, 2000);
+        //        addStage(openingTime + 15  days, 500, 750, 1000);
+        //        addStage(openingTime + 22  days, 100, 100, 100);
+        //        addStage(openingTime + 29  days, 0, 0, 0);
+
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        updatePrice();
     }
 
     // -----------------------------------------
@@ -249,11 +234,40 @@ contract Crowdsale is Ownable{
     }
 
 
-    function addStage(uint _stopDay, uint _bonus) onlyOwner public {
+    function __callback(bytes32 myid, string result, bytes proof) public {
+        if (msg.sender != oraclize_cbAddress()) revert();
+        require (pendingQueries[myid] == true);
+        proof;
+        emit NewKrakenPriceTicker(result);
+        uint USD = parseInt(result);
+        tokenPriceInWei = 1 ether / USD;
+        updatePrice();
+        delete pendingQueries[myid];
+    }
+
+
+    function updatePrice() public payable {
+        if (oraclize_getPrice("URL") > address(this).balance) {
+            emit NewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            emit NewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+
+            bytes32 queryId = oraclize_query(14400, "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0");
+            pendingQueries[queryId] = true;
+        }
+    }
+
+
+    function addStage(uint _stopDay, uint _bonus1, uint _bonus2, uint _bonus3) onlyOwner public {
         require(_stopDay > stages[stageCount].stopDay);
         stageCount++;
         stages[stageCount].stopDay = _stopDay;
-        stages[stageCount].bonus = _bonus;
+        stages[stageCount].bonus1 = _bonus1;
+        stages[stageCount].bonus2 = _bonus2;
+        stages[stageCount].bonus3 = _bonus3;
+        if (closingTime < _stopDay) {
+            closingTime = _stopDay;
+        }
     }
 
     /**
@@ -265,19 +279,40 @@ contract Crowdsale is Ownable{
         uint256 weiAmount = msg.value;
         _preValidatePurchase(_beneficiary, weiAmount);
 
-        // calculate token amount to be created
-        uint256 tokens = _getTokenAmount(weiAmount);
+        uint tokens = 0;
+        uint bonusTokens = 0;
+        uint totalTokens = 0;
 
-        // update state
-        weiRaised = weiRaised.add(weiAmount);
+        (tokens, bonusTokens, totalTokens) = _getTokenAmount(weiAmount);
 
-        _processPurchase(_beneficiary, tokens);
-        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+        _validatePurchase(tokens);
 
-        //_updatePurchasingState(_beneficiary, weiAmount);
+        uint256 price = tokens.div(1 ether).mul(tokenPriceInWei);
 
-        _forwardFunds();
-        //_postValidatePurchase(_beneficiary, weiAmount);
+        uint256 _diff =  weiAmount.sub(price);
+
+        if (_diff > 0) {
+            weiAmount = weiAmount.sub(_diff);
+            msg.sender.transfer(_diff);
+        }
+
+        _processPurchase(_beneficiary, totalTokens);
+        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens, bonusTokens);
+
+        _updateState(weiAmount, totalTokens);
+
+        _forwardFunds(weiAmount);
+    }
+
+
+    function manualSale(address _beneficiary, uint256 _tokens) onlyOwner external {
+        require(_beneficiary != address(0));
+        _validatePurchase(_tokens);
+        uint256 weiAmount = _tokens.mul(tokenPriceInWei);
+
+        _processPurchase(_beneficiary, _tokens);
+        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, _tokens, 0);
+        _updateState(weiAmount, _tokens);
     }
 
     // -----------------------------------------
@@ -291,18 +326,17 @@ contract Crowdsale is Ownable{
      */
     function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) onlyWhileOpen internal view{
         require(_beneficiary != address(0));
+        require(KYC[_beneficiary]);
         require(_weiAmount != 0);
-        require(weiRaised.add(_weiAmount) <= cap);
+        require(tokensSold < cap);
     }
 
-    /**
-     * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
-     * @param _beneficiary Address performing the token purchase
-     * @param _weiAmount Value in wei involved in the purchase
-     */
-    // function _postValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-    //     // optional override
-    // }
+
+    function _validatePurchase(uint256 _tokens) internal view {
+        require(_tokens >= 50 ether && _tokens <= 100000 ether);
+        require(tokensSold.add(_tokens) <= cap);
+    }
+
 
     /**
      * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
@@ -310,7 +344,7 @@ contract Crowdsale is Ownable{
      * @param _tokenAmount Number of tokens to be emitted
      */
     function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
-        token.safeTransfer(_beneficiary, _tokenAmount);
+        token.safeOwnerTransfer(_beneficiary, _tokenAmount);
     }
 
     /**
@@ -322,28 +356,45 @@ contract Crowdsale is Ownable{
         _deliverTokens(_beneficiary, _tokenAmount);
     }
 
-    /**
-     * @dev Override for extensions that require an internal state to check for validity (current user contributions, etc.)
-     * @param _beneficiary Address receiving the tokens
-     * @param _weiAmount Value in wei involved in the purchase
-     */
-    // function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal {
-    //     // optional override
-    // }
 
     /**
      * @dev Override to extend the way in which ether is converted to tokens.
      * @param _weiAmount Value in wei to be converted into tokens
      * @return Number of tokens that can be purchased with the specified _weiAmount
      */
-    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+
+    function _getTokenAmount(uint256 _weiAmount) internal returns (uint,uint,uint) {
+        uint tokens = _weiAmount.div(tokenPriceInWei).mul(1 ether);
 
         if (stages[currentStage].stopDay <= now) {
             _updateCurrentStage();
         }
 
-        return _weiAmount.mul(rate);
+        uint bonus = 0;
 
+        if (_weiAmount < 10 ether) {
+            bonus = stages[currentStage].bonus1;
+        }
+
+        if (_weiAmount >= 10 ether && _weiAmount < 100 ether) {
+            bonus = stages[currentStage].bonus2;
+        }
+
+        if (_weiAmount >= 100 ether) {
+            bonus = stages[currentStage].bonus3;
+        }
+
+        bonus = tokens.mul(bonus).div(10000);
+
+        uint total = tokens.add(bonus);
+
+        if (tokensSold.add(total) > cap) {
+            total = cap.sub(tokensSold);
+            bonus = total.mul(bonus).div(10000 + bonus);
+            tokens = total.sub(bonus);
+        }
+
+        return (tokens, bonus, total);
     }
 
 
@@ -356,11 +407,17 @@ contract Crowdsale is Ownable{
         }
     }
 
+
+    function _updateState(uint256 _weiAmount, uint256 _tokens) internal {
+        weiRaised = weiRaised.add(_weiAmount);
+        tokensSold = tokensSold.add(_tokens);
+    }
+
     /**
      * @dev Overrides Crowdsale fund forwarding, sending funds to escrow.
      */
-    function _forwardFunds() internal {
-        wallet.transfer(msg.value);
+    function _forwardFunds(uint _weiAmount) internal {
+        wallet.transfer(_weiAmount);
     }
 
     /**
@@ -402,7 +459,10 @@ contract Crowdsale is Ownable{
      * executed entirely.
      */
     function finalization() internal {
-        //TODO SEND TOKENS TO RESERFFOUND
+        if (token.balanceOf(this) > 0) {
+            token.safeOwnerTransfer(reserveFund, token.balanceOf(this));
+        }
+        require(token.unpause());
     }
 
 
@@ -411,4 +471,29 @@ contract Crowdsale is Ownable{
         uint _amount = _value.mul(_percent).div(10000);
         return (_amount);
     }
+
+    function addKYC(address _user) onlyOwner public {
+        KYC[_user] = true;
+    }
+
+    function delKYC(address _user) onlyOwner public {
+        KYC[_user] = false;
+    }
+
+
+    function addBalanceForOraclize() payable external {
+        oraclizeBalance = oraclizeBalance.add(msg.value);
+    }
+
+
+    function withdrawBalance() onlyOwner external {
+        require(address(this).balance > 0);
+        owner.transfer(address(this).balance);
+    }
+
+
+    function setGasPrice(uint _newPrice) public onlyOwner {
+        oraclize_setCustomGasPrice(_newPrice * 1 wei);
+    }
+
 }
